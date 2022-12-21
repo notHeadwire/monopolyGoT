419f8b104f63a3db7d696a3716522a3a2ae2f450
oddo <geiger-franzotto@t-online.de>
2022-12-21 11:38:10 +0100

    Moneyanzeige eingebaut

--- Assets/Money/money.js
+++ Assets/Money/money.js
@@ -1,6 +1,5 @@
+//Geld anzeigen
 
-
-document.getElementById('moneyDisplay');
 let moneyDisplay=10000;
 
-document.write(moneyDisplay + "$");
+document.querySelector('#moneyDisplay').innerHTML = 'Player 1: ' + moneyDisplay + "$";
