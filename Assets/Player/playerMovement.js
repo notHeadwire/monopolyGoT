17fc524b18645430f08b7305b78ac3bb1d44a5b8
oddo <geiger-franzotto@t-online.de>
2023-02-06 13:36:29 +0100

    Player Movement nach DiceRoll

--- Assets/Player/playerMovement.js
+++ Assets/Player/playerMovement.js
@@ -1,6 +1,13 @@
-
-
-function movement (){
-
-    document.getElementById("").id = 'redSquare';
+// Function to switch the object to a different div
+function movePlayer(newDivId) {
+    var object = document.getElementById("Player1");
+    var currentDiv = object.parentNode;
+    var newDiv = document.getElementById(newDivId);
+    newDiv.appendChild(object);
 }
+
+// Listener für On-Click Roll Dice
+document.getElementById("Player1").addEventListener("click", function() {
+    var eyes = rollDice();
+    movePlayer(eyes);
+});
